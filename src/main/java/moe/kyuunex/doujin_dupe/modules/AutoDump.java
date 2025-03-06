03a8dd8329fc500290385361efe332926a4843d4
Kyuunex <kyuunex@protonmail.ch>
2025-03-06 15:34:36 +0400

    more default items in auto dump

--- src/main/java/moe/kyuunex/doujin_dupe/modules/AutoDump.java
+++ src/main/java/moe/kyuunex/doujin_dupe/modules/AutoDump.java
@@ -28,7 +28,25 @@ public class AutoDump extends Module {
             new ItemListSetting.Builder()
                 .name("items")
                 .description("A list of items to dump.")
-                .defaultValue(Items.SHULKER_BOX)
+                .defaultValue(
+                    Items.SHULKER_BOX,
+                    Items.WHITE_SHULKER_BOX,
+                    Items.ORANGE_SHULKER_BOX,
+                    Items.MAGENTA_SHULKER_BOX,
+                    Items.LIGHT_BLUE_SHULKER_BOX,
+                    Items.YELLOW_SHULKER_BOX,
+                    Items.LIME_SHULKER_BOX,
+                    Items.PINK_SHULKER_BOX,
+                    Items.GRAY_SHULKER_BOX,
+                    Items.LIGHT_GRAY_SHULKER_BOX,
+                    Items.CYAN_SHULKER_BOX,
+                    Items.PURPLE_SHULKER_BOX,
+                    Items.BLUE_SHULKER_BOX,
+                    Items.BROWN_SHULKER_BOX,
+                    Items.GREEN_SHULKER_BOX,
+                    Items.RED_SHULKER_BOX,
+                    Items.BLACK_SHULKER_BOX
+                )
                 .build());
     public final Setting<List<ScreenHandlerType<?>>> screens =
         sgDefault.add(
