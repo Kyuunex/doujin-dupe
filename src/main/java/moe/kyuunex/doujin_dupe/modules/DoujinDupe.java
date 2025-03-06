836a48c321990500ba8b97c6677ffc8965503527
Kyuunex <kyuunex@protonmail.ch>
2025-03-06 16:47:51 +0400

    using accesswidener was overkill ngl. no more

--- src/main/java/moe/kyuunex/doujin_dupe/modules/DoujinDupe.java
+++ src/main/java/moe/kyuunex/doujin_dupe/modules/DoujinDupe.java
@@ -179,9 +179,6 @@ public class DoujinDupe extends Module {
         ClientPacketListener network = mc.getConnection();
         if (network == null) return;
 
-        Connection connection = network.getConnection();
-        if (connection == null) return;
-
-        connection.channel.writeAndFlush(packet);
+        network.getConnection().send(packet, null, true);
     }
 }
